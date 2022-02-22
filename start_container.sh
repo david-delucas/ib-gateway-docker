3ea8e50466881a896d05718fff5445c56683ccef
David de Lucas <delucas.david@gmail.com>
2022-02-22 09:29:04 +0100

    TG-174 misc stuff

--- start_container.sh
+++ start_container.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-sudo docker run -d -p 4002:4002 -p 5900:5900 --env-file ~/env.list  ib-gateway-docker
+sudo docker run -d -p 4002:4002 -p 5900:5900 --name ibgtw --env-file ~/env.list  ib-gateway-docker
--- stop_container.sh
+++ stop_container.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
 sudo docker stop $(sudo docker ps -a -q --filter ancestor=ib-gateway-docker --format="{{.ID}}")
+sudo docker rm $(sudo docker ps -a -q --filter ancestor=ib-gateway-docker --format="{{.ID}}")
